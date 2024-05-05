2ad7e471c630efdb3b382e7176efc5b1a4c73705
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-05-05 06:28:22 +0200

    Added EFRaw queries

--- src/POC_ORM_NetCore/Program.cs
+++ src/POC_ORM_NetCore/Program.cs
@@ -6,12 +6,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Model;
 
 namespace POC_ORM_NetCore
 {
     internal class Program
     {
-        private static List<long> _results;
+        private static List<long> _resultsDapper;
+        private static List<long> _resultsEF;
+        private static List<long> _resultsEFRaw;
+        private static List<long> _resultsNHibernate;
         private static Stopwatch sw;
 
         private static async Task Main(string[] args)
@@ -26,14 +30,44 @@ namespace POC_ORM_NetCore
             Console.WriteLine("Dapper");
             await StartTestDapperAsync();
 
-            //Console.WriteLine("ADO");s
-            //StartTestAdo();
-
             Console.WriteLine("EntityFramework");
             await StartTestEfAsync();
 
+            Console.WriteLine("EntityFramework Raw");
+            await StartTestEfRawAsync();
+
             Console.WriteLine("NHibernate");
             await StartTestNHibernateAsync();
+
+            double avgDapper = _resultsDapper.Average();
+            double avgEF = _resultsEF.Average();
+            double avgEFRaw = _resultsEFRaw.Average();
+            double avgNHibernate = _resultsNHibernate.Average();
+
+            // Calculate percentage differences
+            double diffEF = ((avgEF - avgDapper) / avgDapper) * 100;
+            double diffEFRaw = ((avgEFRaw - avgDapper) / avgDapper) * 100;
+            double diffNHibernate = ((avgNHibernate - avgDapper) / avgDapper) * 100;
+
+            // Print results
+            Console.WriteLine("Average Response Times:");
+            Console.WriteLine($"Dapper: {avgDapper} ms");
+
+            if (diffEF < 0)
+                Console.WriteLine($"EF: {avgEF} ms ({Math.Round(Math.Abs(diffEF), 2)}% faster than Dapper)");
+            else
+                Console.WriteLine($"EF: {avgEF} ms ({Math.Round(diffEF, 2)}% slower than Dapper)");
+
+            if (diffEFRaw < 0)
+                Console.WriteLine($"EF Raw: {avgEFRaw} ms ({Math.Round(Math.Abs(diffEFRaw), 2)}% faster than Dapper)");
+            else
+                Console.WriteLine($"EF Raw: {avgEFRaw} ms ({Math.Round(diffEFRaw, 2)}% slower than Dapper)");
+
+            if (diffNHibernate < 0)
+                Console.WriteLine($"NHibernate: {avgNHibernate} ms ({Math.Round(Math.Abs(diffNHibernate), 2)}% faster than Dapper)");
+            else
+                Console.WriteLine($"NHibernate: {avgNHibernate} ms ({Math.Round(diffNHibernate, 2)}% slower than Dapper)");
+
         }
 
         private static async Task StartTestDapperAsync()
@@ -41,71 +75,85 @@ namespace POC_ORM_NetCore
             var objDapper = new Data.Dapper.PersonRepository();
             sw = new Stopwatch();
 
-            await objDapper.WarmUpAsync();
-
-            _results = new List<long>();
+            for (int i = 0; i < 3; i++)
+            {
+                await objDapper.WarmUpAsync();
+            }
+            
+            _resultsDapper = new List<long>();
 
             for (int i = 0; i < 100; i++)
             {
                 sw.Start();
                 var ret = await objDapper.GetAllPersonAsync();
                 sw.Stop();
-                _results.Add(sw.ElapsedMilliseconds);
-                Console.WriteLine($"{sw.ElapsedMilliseconds} ms Records: {ret.Length}");
+                _resultsDapper.Add(sw.ElapsedMilliseconds);
+                Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
                 sw.Reset();
             }
 
-            Console.WriteLine($"Average: {_results.Average()}");
-        }
-
-        private static async Task StartTestAdoAsync()
-        {
-            var objAdo = new Data.ADO.PersonRepository();
-            sw = new Stopwatch();
-
-            await objAdo.WarmUpAsync();
-
-            _results = new List<long>();
-
-            for (int i = 0; i < 100; i++)
-            {
-                sw.Start();
-                var ret = await objAdo.GetAllPersonAsync();
-                sw.Stop();
-                _results.Add(sw.ElapsedMilliseconds);
-                Console.WriteLine(sw.ElapsedMilliseconds + "ms");
-                sw.Reset();
-            }
-            Console.WriteLine($"Average: {_results.Average()}");
+            Console.WriteLine($"Average: {_resultsDapper.Average()}");
         }
 
         private static async Task StartTestEfAsync()
         {
             sw = new Stopwatch();
 
-            _results = new List<long>();
+            _resultsEF = new List<long>();
 
             //WarmUp
-
-            await using (var context = new DataContext())
+            for (int i = 0; i < 3; i++)
             {
-                var x = await context.Person.FirstOrDefaultAsync();
+                await using (var context = new DataContext())
+                {
+                    var x = await context.Person.FirstOrDefaultAsync();
+                }
             }
 
             for (int i = 0; i < 100; i++)
             {
                 sw.Start();
 
+                await using var context = new DataContext();
+                var ret = await context.Person.ToArrayAsync();
+                sw.Stop();
+                _resultsEF.Add(sw.ElapsedMilliseconds);
+                Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+                sw.Reset();
+            }
+
+            Console.WriteLine($"Average: {_resultsEF.Average()}");
+        }
+
+        private static async Task StartTestEfRawAsync()
+        {
+            sw = new Stopwatch();
+
+            _resultsEFRaw = new List<long>();
+
+            //WarmUp
+
+            for (int i = 0; i < 3; i++)
+            {
                 await using (var context = new DataContext())
                 {
-                    var ret = await context.Person.ToArrayAsync();
-                    sw.Stop();
-                    _results.Add(sw.ElapsedMilliseconds);
-                    Console.WriteLine(sw.ElapsedMilliseconds + "ms");
-                    sw.Reset();
+                    var x = await context.Database.SqlQuery<Person>($"select * from Person").ToListAsync();
                 }
             }
-            Console.WriteLine($"Average: {_results.Average()}");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Start();
+
+                await using var context = new DataContext();
+                var ret = await context.Database.SqlQuery<Person>($"select * from Person").ToListAsync();
+                sw.Stop();
+                _resultsEFRaw.Add(sw.ElapsedMilliseconds);
+                Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+                sw.Reset();
+            }
+
+            Console.WriteLine($"Average: {_resultsEFRaw.Average()}");
         }
 
         private static async Task StartTestNHibernateAsync()
@@ -113,19 +161,24 @@ namespace POC_ORM_NetCore
             var objNh = new Data.NHibernate.PersonRepository();
             sw = new Stopwatch();
 
-            _results = new List<long>();
+            _resultsNHibernate = new List<long>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                var ret = await objNh.GetAllPersonAsync();
+            }
 
             for (int i = 0; i < 100; i++)
             {
                 sw.Start();
                 var ret = await objNh.GetAllPersonAsync();
                 sw.Stop();
-                _results.Add(sw.ElapsedMilliseconds);
+                _resultsNHibernate.Add(sw.ElapsedMilliseconds);
                 Console.WriteLine(sw.ElapsedMilliseconds + "ms");
                 sw.Reset();
             }
 
-            Console.WriteLine($"Average: {_results.Average()}");
+            Console.WriteLine($"Average: {_resultsNHibernate.Average()}");
         }
     }
 }
